0b8d0f051d8c581a34f5184fc309c69b305531ed
Clement Li <clem.li.kh@gmail.com>
2014-09-21 22:24:28 +0100

    Update cachematrix.R
    makeCacheMatrix function update

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,31 @@
 ## Coursera R Programming Assignment 2: Lexical Scoping
 ## Caching the Inverse of a Matrix
 
-## This function creates a special "matrix" object that
+## makeCacheMatrix creates a special "matrix" object that
 ## can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+      inv <- NULL            #
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x 
+      setinverse <- function(inverse) inv <<- inverse
+      getinverse <- function() inv
+      list(set = set, get = get, 
+           setinverse = setinverse, 
+           getinverse = getinverse)
       
-}
-
+}      
+      
+      
 
 ## This function computes 
 
 cacheSolve <- function(x, ...) {
       ## Return a matrix that is the inverse of 'x'
+      
+      
+      
 }
-
-
-## test of git push
